ddaf5f3409b95eac1fca9270c5893d558556ff5d
Patrick Loftus <DonLofto@users.noreply.github.com>
2023-11-04 14:17:26 +0000

    Testing logic changes

--- src/main/java/com/example/project/PetitionController.java
+++ src/main/java/com/example/project/PetitionController.java
@@ -11,10 +11,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 
+/**
+ * The type Petition controller.
+ */
 @Controller
 public class PetitionController {
    private final PetitionService petitionService;
 
+    /**
+     * Instantiates a new Petition controller.
+     *
+     * @param petitionService the petition service
+     */
     public PetitionController(PetitionService petitionService) {
         this.petitionService = petitionService;
     }
@@ -24,17 +32,37 @@ public class PetitionController {
         return "create";
     }*/
 
+    /**
+     * Create petition string.
+     *
+     * @param title       the title
+     * @param description the description
+     * @return the string
+     */
     @PostMapping("/create")
     public String createPetition(@RequestParam String title, @RequestParam String description){
         Petition newPetition= new Petition(title, description);
         petitionService.addPetition(newPetition);
-        return "Create";
+        return "index";
     }
+
+    /**
+     * Search form string.
+     *
+     * @return the string
+     */
     @GetMapping("/search")
     public String searchForm() {
         return "search";
     }
 
+    /**
+     * Search result string.
+     *
+     * @param query the query
+     * @param model the model
+     * @return the string
+     */
     @PostMapping("/searchResult")
     public String searchResult(@RequestParam String query, Model model) {
         List<Petition> results = petitionService.searchPetitions(query);
@@ -43,6 +71,13 @@ public class PetitionController {
     }
 
 
+    /**
+     * View petition string.
+     *
+     * @param id    the id
+     * @param model the model
+     * @return the string
+     */
     @GetMapping("/petition/{id}")
     public String viewPetition(@PathVariable int id, Model model) {
         model.addAttribute("petition", petitionService.getPetitionById(id));
