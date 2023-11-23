73a869df7ed1daedb4943c961f66256f17dc5c9b
Patrick Loftus <DonLofto@users.noreply.github.com>
2023-11-23 20:24:38 +0000

    Changes to schema and application properties

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,12 +10,13 @@ CREATE TABLE petition (
 
 -- Creating the signature table
 CREATE TABLE signature (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    petition_id BIGINT,
-    email VARCHAR(255),
-    name VARCHAR(255),
-    PRIMARY KEY (id)
-);;
+   id BIGINT NOT NULL AUTO_INCREMENT,
+   petition_id BIGINT,
+   email VARCHAR(255),
+   name VARCHAR(255),
+   PRIMARY KEY (id),
+   FOREIGN KEY (petition_id) REFERENCES petition (id)
+);
 
 -- Creating the petition_signatures table
 CREATE TABLE petition_signatures (
@@ -36,4 +37,21 @@ ADD CONSTRAINT fk_petition_petition_id FOREIGN KEY (petition_id) REFERENCES peti
 
 ALTER TABLE signature 
 ADD CONSTRAINT fk_petition_in_signature FOREIGN KEY (petition_id) REFERENCES petition (id);;
+
+INSERT INTO petition (description, title)
+VALUES
+    ('Improve Public Transportation System', 'Public Transportation Enhancement Petition'),
+    ('Renovate Local Community Center', 'Community Center Renovation Petition'),
+    ('Improve Local Playground', 'Local Playground Improvement Petition'),
+    ('Improve Local Library', 'Local Library Improvement Petition');
+
+INSERT INTO signature (petition_id, email, name)
+VALUES
+    (1, 'signature1@example.com', 'John Doe'),
+    (1, 'signature2@example.com', 'Jane Smith');
+
+
+
+
+
 
